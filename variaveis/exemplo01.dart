bbf8509209f0c5fc4b1c0411a71a021e1f51281e
Guilherme <alonsogui011@gmail.com>
2023-08-08 10:38:46 -0300

    new feature

---
+++ variaveis/exemplo01.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 30; //numero inteiro
+  double pi = 3.14; // número com vírgula
+  bool isDartFun = true; // tipo bool, ou seja, verdadeiro ou falso
+  String name = 'John'; // para texto
+  List<int> numbers = [1, 2, 3];
+
+  print(age);
+  print(pi);
+  print(isDartFun);
+  print(name);
+  print("----------");
+}
